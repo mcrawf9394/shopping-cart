4f80d7c92eae313aeb25501e604eb9807527ca7f
mcrawf9394 <mjc9394@gmail.com>
2024-05-14 21:13:23 -0400

    implemented the async function that accesses the fake store api and made basic display of the information

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
-  return ( <>
-    <h1 className='text-5xl'>Mock Shop</h1>
-    <nav>
-      <Link to="store">Store</Link>
-      <Link to="cart">Shopping Cart</Link>
+  return ( <div>
+    <nav className='grid grid-cols-3 grid-rows-1 items-center bg-indigo-500 py-5'>
+      <h1 className='text-5xl text-center'>Mock Shop</h1>
+      <Link to="store" className='text-2xl hover:text-white text-center'>Store</Link>
+      <Link to="cart"className='text-2xl hover:text-white text-center'>Shopping Cart</Link>
     </nav>
     <Outlet />
-    </>
+    </div>
   )
 }
 
--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react"
+import { useState } from "react"
 export default function Homepage () {
-    return <>
-        <h2>Some stuff</h2>
-    </>
+    const [items, setItems] = useState(["loading"])
+    useEffect(() => {
+        const itemSource = async function () {
+            let source = await fetch('https://fakestoreapi.com/products')
+            let info = await source.json()
+            setItems(info)
+        }
+        itemSource ()
+    }, [])
+    return <div className="grid grid-cols-4 grid-rows-5 m-10 gap-4">
+        {items.map((item) =>{
+            if (item === "loading") {
+                return <>
+                    <h3>loading</h3>
+                </>
+            }
+            else {
+                return <div>
+                    <img src={item.image} alt="photo of product" className="size-20"/>
+                    <h4>{item.title}</h4>
+                    <h5>{item.price}</h5>
+                </div>
+            }
+        })}
+    </div>
 }
